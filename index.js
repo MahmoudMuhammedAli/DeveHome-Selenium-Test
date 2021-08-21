871a8cdd71d6fa34243fcfb18a072bad4286b1e7
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2021-08-21 03:18:36 +0200

    Example test now working

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const {Builder, By , Key,  Util }  = require("selenium-webdriver");
 async function example () {
-  let driver = await new Builder().forBrowser("Firefox") . build();
+  let driver = await new Builder().forBrowser("firefox").build();
   await driver.get("http://google.com");
-  await driver.findElement(By.name("q").sendKeys("Selenium" , Key.RETURN));
+  await driver.findElement(By.name("q")).sendKeys("Selenium" , Key.RETURN);
 
 }
 example()
